65d4f33a8f298e04710639511a3665a39b9a1c49
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2023-02-13 01:07:08 +0200

    add table db

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1:3306;dbname=yii2basic',
+    'dsn' => 'mysql:host=127.0.0.1:3306;dbname=basic',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
